6bc3b67eaed7fe35fa4239c30c2192669ecdd6f1
Saurabh Patel <38186468+Saurabhrkp@users.noreply.github.com>
2020-04-08 01:39:41 +0530

    Added NextApp in express App

--- app.js
+++ app.js
@@ -2,10 +2,16 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const next = require('next');
+const dev = process.env.NODE_DEV !== 'production'; //true false
+
+const nextApp = next({ dev });
+const handle = nextApp.getRequestHandler(); //part of next config
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+nextApp.prepare();
 var app = express();
 
 app.use(logger('dev'));
@@ -13,8 +19,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-module.exports = app;
+app.get('*', (req, res) => {
+  return handle(req, res); // for all the react stuff
+});
+
+module.exports = { app: app, handle: handle };
